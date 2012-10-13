31f43191450192e2fcdb1d9fae8265f3eef110c9
Daniele Tricoli <eriol@mornie.org>
2012-10-13 05:54:14 +0200

    Enhanced version management

--- pypel/__init__.py
+++ pypel/__init__.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*-
+"""pypel package, simple receipts management tool.
 
-__version__ = '0.1'
+THIS SOFTWARE IS UNDER BSD LICENSE.
+Copyright (c) 2012 Daniele Tricoli <eriol@mornie.org>
+
+Read LICENSE for more informations.
+"""
+
+VERSION = (0, 1)
+
+def get_version():
+    """Returns project version in a human readable form."""
+    return '.'.join(str(v) for v in VERSION)
