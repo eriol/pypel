83938c9bf7aafc1f7a2a6b9594279600012ee7ef
Daniele Tricoli <eriol@mornie.org>
2015-06-26 04:21:11 +0200

    Update Python supported versions classifiers

--- setup.py
+++ setup.py
@@ -29,8 +29,7 @@ Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.2
-Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
 Topic :: Office/Business :: Financial
 '''.strip().splitlines()
 
